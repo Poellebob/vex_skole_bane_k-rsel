e4b05cd9438fc57fd6aac37d26ab6e5e3dbea96e
Viggo Kirkegaard Helstrup <vkirkegaardhelstrup@gmail.com>
2024-12-19 08:45:34 +0100

    Add README and update motor configurations in main.cpp

---
+++ README.md
@@ -0,0 +1,28 @@
+# Use and upload the code to vex v5
+
+To use this code in Visual Studio Code (VSCode) with the PROS extension, follow these steps:
+
+1. **Install VSCode**: If you haven't already, download and install Visual Studio Code from [here](https://code.visualstudio.com/).
+
+2. **Install the PROS Extension**:
+
+   - Open VSCode.
+   - Go to the Extensions view by clicking on the Extensions icon in the Activity Bar on the side of the window or by pressing `Ctrl+Shift+X`.
+   - Search for "PROS" and install the PROS extension.
+
+3. **Open the Project**:
+
+   - Download your project as a .zip file from GitHub by clicking the green "Code" button, then selecting "Download ZIP".
+   - Extract the downloaded .zip file to a directory of your choice.
+   - Open the cloned project folder in VSCode by going to `File > Open Folder...` and select the folder with the code.
+
+4. **Build the Project**:
+
+   - Find the `src/main.cpp` and open it by clicking on it.
+   - Connect the vex v5 brain to your computer via usb.
+   - Find the PROS "build and upload" button and click on it.
+
+5. **Run the Code**:
+   - Press the play button on the v5 brain.
+
+For more detailed information, refer to the [PROS documentation](https://pros.cs.purdue.edu/v5/getting-started/index.html).
--- src/main.cpp
+++ src/main.cpp
@@ -1,44 +1,34 @@
 #include "main.h"
 #include "vector"
 // #include "math.h"
-//  hvis den fejler upload pårund af permistions så prøv
-//  sudo chmod 666 <port>
 
-pros::Motor left_mtr(2);
-pros::Motor right_mtr(-1);
+// spesify the ports for the motors and put "-" for ones that are reversed
+pros::Motor left_mtr(-1);
+pros::Motor right_mtr(11);
 
 /*
 line: distance(cm), speed(rpm)
 circ: angle, dir(left or right), radius(cm), speed(rpm)
-loop
+loop; this will loop the path back to the start
 */
 std::vector<std::string> path = {
-	"line: 100, 50",
-	"circ: 180, right, 50, 50",
-	"loop"};
+	// exemple path
+	"line: 190, 40",
+	"circ: 93, right, 30, 40",
+	"line: 220, 40",
+	"circ: 160, left, 60, 40",
+	"line: 100, 40",
+	"circ: 75, right, 45, 40",
+	"line: 120, 40",
+	"circ: 180, left, 58, 40",
+	"circ: 105, right, 50, 40",
+	"line: 45, 40",
+	"circ: 90, left, 60, 40",
+	"line: 400, 40"};
 
+// spesify the wheel circumference and the width of the robot in cm
 float wheel_circumference = 10.3 * M_PI;
-int robot_width = 30;
-
-/**
- * A callback function for LLEMU's center button.
- *
- * When this callback is fired, it will toggle line 2 of the LCD text between
- * "I was pressed!" and nothing.
- */
-void on_center_button()
-{
-	static bool pressed = false;
-	pressed = !pressed;
-	if (pressed)
-	{
-		pros::lcd::set_text(2, "I was pressed!");
-	}
-	else
-	{
-		pros::lcd::clear_line(2);
-	}
-}
+float robot_width = 34;
 
 /**
  * Runs initialization code. This occurs as soon as the program is started.
@@ -49,9 +39,6 @@ void on_center_button()
 void initialize()
 {
 	pros::lcd::initialize();
-	pros::lcd::set_text(1, "Hello PROS User!");
-
-	pros::lcd::register_btn1_cb(on_center_button);
 }
 
 /**
@@ -83,7 +70,10 @@ void competition_initialize() {}
  * will be stopped. Re-enabling the robot will restart the task, not re-start it
  * from where it left off.
  */
-void autonomous() {}
+void opcontrol()
+{
+	autonomous();
+}
 
 /**
  * Runs the operator control code. This function will be started in its own task
@@ -98,30 +88,38 @@ void autonomous() {}
  * operator control task will be stopped. Re-enabling the robot will restart the
  * task, not resume it from where it left off.
  */
-void opcontrol()
+void autonomous()
 {
 	for (int i = 0; i < path.size(); i++)
 	{
+		// get the type of path
 		std::string path_type = path[i].substr(0, 4);
 		pros::lcd::set_text(1, path[i].substr(0, 4));
 
 		if ("loop" == path_type)
 		{
-			i = 0;
+
+			i = -1;
 		}
 		if ("line" == path_type)
 		{
+			// get all the arguments for a line path
 			std::string args = path[i].substr(5);
 			int comma_pos = args.find(",");
 			float distance = std::stoi(args.substr(0, comma_pos));
 			float speed = std::stoi(args.substr(comma_pos + 1));
+
+			// move the robot at the given speed in rpm
 			left_mtr.move_velocity(speed);
 			right_mtr.move_velocity(speed);
+
+			// calculate the time it will take to move the given distance
 			float time = (distance / wheel_circumference) / speed;
 			pros::delay(time * 60000);
 		}
 		if ("circ" == path_type)
 		{
+			// get all the arguments for a circle path
 			std::string args = path[i].substr(5);
 			int comma_pos = args.find(",");
 			float angle = std::stoi(args.substr(0, comma_pos));
@@ -132,6 +130,13 @@ void opcontrol()
 			comma_pos = args.find(",");
 			float radius = std::stoi(args.substr(0, comma_pos));
 			float speed = std::stoi(args.substr(comma_pos + 1));
+
+			// figure out which way to turn the robot
+			// and move the robot at the given speed in rpm with the correct ratio
+
+			// the radius is calculated from the center of the robot
+			// and the speed is calculated from the outer wheel
+			// the inner wheel will move at the speed * (the radius of the inner wheels path) / (the radius of the outer wheels path)
 			if ("left" == dir || " left" == dir)
 			{
 				left_mtr.move_velocity(speed);
@@ -142,10 +147,13 @@ void opcontrol()
 				right_mtr.move_velocity(speed);
 				left_mtr.move_velocity(speed * (radius - (robot_width / 2)) / (radius + (robot_width / 2)));
 			}
+
+			// calculate the time it will take for the outer wheel to move the given angle
 			float time = (angle / 360) * (((2 * M_PI * (radius + (robot_width / 2))) / (wheel_circumference)) / speed) * 60;
 			pros::delay(time * 1000);
 		}
 	}
+	// stop the robot at the end of the path
 	right_mtr.move_velocity(0);
 	left_mtr.move_velocity(0);
 }
